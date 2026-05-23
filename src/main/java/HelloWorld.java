2863b5d24509d1b400d74b8fcd30da94acf74da5
sanjai361 <sanjaisri361@gmail.com>
2026-05-23 17:59:51 +0530

    new commit was happened

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven Project!");
+        System.out.println("Hello from mars  Project!");
     }
 }
